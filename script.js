62ba8208c83d21b023da56023f96de56f0247b63
Nima Mehrjoo <nmehrjoo1999@gmail.com>
2021-12-10 18:55:05 -0500

    set up the functionality of the quiz

--- script.js
+++ script.js
@@ -1,5 +1,87 @@
-const quizCOntainer = document.getElementById('quiz');
-const resultsCOntainer = document.getElementById('results');
+
+function buildQuiz(){
+    //varuiable to store html output
+    const output = [];
+
+    output.push(
+        `<div class="slide"> 
+            <button class="start"> click to start </button>
+        </div>`
+    )
+    //for each question...
+    myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+
+            //variable to store the list of possible answers
+            const answersArray = [];
+
+            for(letter in currentQuestion.answers){
+
+                //... add an html radio button
+                answersArray.push(
+                    `<label>
+                    <input type="radio" name="question${questionNumber}" value="${letter}">
+                    ${currentQuestion.answers[letter]}
+                    </label>`
+                )
+            }
+            //... add this question and its answers to the output
+            output.push(
+                `<div class="slide">
+                <div class="question"> ${currentQuestion.question} </div>
+                <div class="answers"> ${answersArray.join('')} </div>
+                </div>`
+            );
+        }
+
+    );
+    quizContainer.innerHTML = output.join('');
+
+}
+
+function showResults(){
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+    //keep track ofthe correct answers 
+    let numCorrect = 0;
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+        
+        // find selected answer
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+        // if answer is correct 
+        if(userAnswer === currentQuestion.correctAnswer){
+            //add to the number of correct answers
+            numCorrect++;
+
+            //color the answers green 
+            answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+        //if answer is wrong
+        else{
+            //color the answer red
+            answerContainers[questionNumber].style.color = 'red';
+        }
+    });
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+}
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+}
+function nextSlide(){
+    showSlide(currentSlide +1) ;
+}
+
+
+/*  variables */
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
 const myQuestions = [
     {
@@ -55,12 +137,27 @@ const myQuestions = [
     }
 ];
 
-function buildQuiz(){}
-
-function showResults(){}
-
 //display quiz right away
 buildQuiz();
 
+const slides = document.querySelectorAll(".slide");
+const start = document.querySelector(".start");
+let currentSlide = 0;
+var numQuestion = myQuestions.length;
+debugger;
+
+//show the first slide
+showSlide(currentSlide);
 //on submit, show results
+
 submitButton.addEventListener('click', showResults);
+start.addEventListener('click', startQuiz);
+function startQuiz(){
+    nextSlide();
+    for(var i=0; i<numQuestion; i++){
+        document.querySelectorAll('input[name="question' + i + '"]').forEach((elem) => {
+            elem.addEventListener("change", nextSlide );
+            numm =+ 1;
+        });
+    }
+}
